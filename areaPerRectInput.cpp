f986043d3eaef93fc046cc9dba816a08ee1f7861
Patrice Pat-Odita <patrice.pat-odita@stu.ocsb.ca>
2022-09-25 12:17:08 +0000

    Added areaPerRectInput.cpp

---
+++ areaPerRectInput.cpp
@@ -0,0 +1,30 @@
+// Copyright (c) 2021 Patrice Pat-Odita All rights reserved.
+//
+// Created by: Patrice Pat-odita
+// Created on: Sep. 23, 2022
+// This program asks the user for the length
+// and width of a rectangle. It then calculates
+// and displays the area and perimeter.
+#include <iostream>
+
+// declare variables
+float length, width, area, perimeter;
+
+int main() {
+    // get input from user
+    std::cout << "Today we will calculate the area and\n";
+    std::cout << "perimeter of a rectangle.\n";
+    std::cout << "Enter the length (cm): ";
+    std::cin >> length;
+    std::cout << "Enter the width (cm): ";
+    std::cin >> width;
+
+    // calculate area and perimeter
+    area = length * width;
+    perimeter = 2 * (length + width);
+
+    // display the results to the user
+    std::cout << std::endl;
+    std::cout << "Area = " << area << " cm^2.\n";
+    std::cout << "Perimeter = " << perimeter << "cm.\n";
+}
